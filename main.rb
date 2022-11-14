8d96426b2ced7707c87a76a52f76c8338a84d029
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-11-14 16:53:14 -0700

    Create some helper functions and initialize logic for question/turn loop

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 require "./classes/player"
 require "./classes/question"
 
+def loading
+  sleep(0.2)
+  print "."
+  sleep(0.2)
+  print "."
+  sleep(0.2)
+  print "."
+  puts ""
+end
+
 def players
   puts "Player 1, please enter your name: "
   player1_name = gets.chomp
@@ -17,11 +27,44 @@ def players
   sleep(0.5)
 end
 
+def rules
+  loading
+  puts "Each player begins with 3 lives!"
+  sleep(2)
+  puts "Get a math question wrong, and you lose a life."
+  sleep(2)
+  puts "Survive until your opponent loses all three of their lives, and you win the game!"
+  sleep(2)
+end
+
+def ask_question
+
+end
+
+def game
+  rules
+  @player1_count = 0
+  @player2_count = 0
+
+  while @player1.alive? && @player2.alive?
+    if (@player1_count == @player2_count)
+      @player1_count += 1
+    else
+      @player2_count += 1
+    end
+
+    puts "----- NEW TURN -----"
+    ask_question
+  end
+end
+
 def start
   puts "Welcome to the Ruby OOP, Two Player, Math Game!!!"
   sleep(0.5)
 
   players
+
+  game
 end
 
 start
